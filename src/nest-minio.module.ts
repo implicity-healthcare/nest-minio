dd858d66e70cffed41e8376e38866ee28e59a6df
Julien Sarazin <julien.sarazin@me.com>
2019-08-22 13:28:51 +0200

    updated client instatiation

--- src/nest-minio.module.ts
+++ src/nest-minio.module.ts
@@ -1,20 +1,20 @@
 import { DynamicModule, Global, Module } from '@nestjs/common';
 import { NestMinioClientProviderToken } from './constants';
-import * as Minio from 'minio';
-import { IMinioModuleConfiguration } from './interfaces';
+import { NestMinioClient, NestMinioModuleConfiguration } from './interfaces';
+import { Client } from 'minio';
 
 @Global()
 @Module({})
 export class NestMinioModule {
 
-    static init(configuration: IMinioModuleConfiguration): DynamicModule {
+    static init(configuration: NestMinioModuleConfiguration): DynamicModule {
 
         /**
          * Configure minio client by connecting to the
          */
         const minoClientProvider = {
             provide: NestMinioClientProviderToken,
-            useFactory: (): Minio.Client => new Minio.Client(configuration)
+            useFactory: (): NestMinioClient => new Client(configuration)
         };
 
         return {
